9dd5a7df650e5d97bfed4a274adbfc49decba979
A12 <A12@NOTEA1208.santuario.local>
2025-05-29 18:11:37 -0300

    Deixando o header dinâmico

--- componentes/header.php
+++ componentes/header.php
@@ -1,3 +1,13 @@
+<?php
+    $itens = [
+        ['href' => '#projetos', 'texto' => 'Projetos'],
+        ['href' => '', 'texto' => 'Github'],
+        ['href' => '', 'texto' => 'LinkedIn'],
+        ['href' => '', 'texto' => 'Twitter'],
+    ];
+
+?>
+ 
  <!-- Cabeçalho -->
     <header class="mx-auto max-w-screen-lg px-3 py-6 flex items-center justify-between">
 
@@ -9,10 +19,12 @@
         <!-- Links -->
         <div>
             <ul class="flex gap-x-3 font-medium">
-                <li><a href="#projetos" class="hover:underline">Projetos</a></li>
-                <li><a href="#github" class="hover:underline">Github</a></li>
-                <li><a href="#linkedin" class="hover:underline">LinkedIn</a></li>
-                <li><a href="Twitter" class="hover:underline">Twitter</a></li>
+                
+                <?php foreach ($itens as $item): ?>
+                    <li><a href="<?=$item['href']?>" class="hover:underline"><?=$item['texto']?></a></li>
+
+                <?php endforeach; ?>
+
             </ul>
         </div>
 
